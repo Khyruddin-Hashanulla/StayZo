26e1999fdb38a0a34f5b8499f6a0b98b0ed04221
Khyruddin-Hashanulla <khyruddin.personal@gmail.com>
2025-06-27 01:17:42 +0530

    Add Flash Massage

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const methodOverride = require("method-override");
 const path = require("path");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./Utils/ExpressError.js");
+const session = require("express-session");
+const flash = require("connect-flash");
 
 const listings = require("./Routes/listing.js");
 const reviews = require("./Routes/review.js");
@@ -36,10 +38,34 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "/Public")));
 
+//EXPRESS SESSION
+
+const sessionOptions = {
+  secret: "mysupersecretcode",
+  resave: false,
+  saveUninitialized: true,
+  Cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
+};
+
 // Root Route
 
 app.get("/", (req, res) => {
   res.send("Hello from StayZo Root");
+});
+
+//FLASH
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
 });
 
 //FOR ROUTERS
