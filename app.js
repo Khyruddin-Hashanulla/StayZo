c6c8b3abf38fba439e893a98d613265b646a47c7
Khyruddin-Hashanulla <khyruddin.personal@gmail.com>
2025-07-03 21:34:16 +0530

    Add Final Files but some things is under development

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const path = require("path");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./Utils/ExpressError.js");
 const session = require("express-session");
+const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -50,8 +51,21 @@ app.use(express.static(path.join(__dirname, "/Public")));
 
 //EXPRESS SESSION
 
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  crypto: {
+    secret: process.env.SECRET,
+  },
+  touchAfter: 24 * 3600,
+});
+
+store.on("error", () => {
+  console.log("Error in Mongo Session Store ", err);
+});
+
 const sessionOptions = {
-  secret: "mysupersecretcode",
+  store,
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
   Cookie: {
